2206fc29c04995feec76bcda5cd5a086cee1a3d6
Jose Francisco Vergara Espinoza <j.f.vergara@cgiar.org>
2021-06-07 21:33:40 -0500

    feat: Get products with less than 10 in the inventory finished

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import openpyxl
 inv_file = openpyxl.load_workbook("inventory.xlsx")
 product_list = inv_file["Sheet1"]
 
-products_per_supplier = {}  # dictionary
+products_per_supplier = {}  # empty dictionary
 total_value_per_supplier = {}
+products_under_10_inv = {}
 
 for products_row in range(2, product_list.max_row + 1):
     supplier_name = product_list.cell(products_row, 4).value
     inventory = product_list.cell(products_row, 2).value
     price = product_list.cell(products_row, 3).value
+    product_num = product_list.cell(products_row, 1).value
 
     # Calculation number of products per supplier
     if supplier_name in products_per_supplier:
         current_num_products = products_per_supplier.get(supplier_name)
-        products_per_supplier[supplier_name] = current_num_products +1
+        products_per_supplier[supplier_name] = current_num_products + 1
     else:
         products_per_supplier[supplier_name] = 1
 
@@ -25,8 +27,16 @@ for products_row in range(2, product_list.max_row + 1):
     else:
         total_value_per_supplier[supplier_name] = inventory * price
 
-print("Products per supplier:")
+    # logic products with inventory less than 10
+    if inventory < 10:
+        products_under_10_inv[int(product_num)] = int(inventory)
+print(products_under_10_inv)
+
+
+
+
+"""print("Products per supplier:")
 print(products_per_supplier)
 
 print("Total value per supplier")
-print(total_value_per_supplier)
+print(total_value_per_supplier)"""
